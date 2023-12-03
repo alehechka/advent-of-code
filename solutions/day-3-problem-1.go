d55f78b9bbf7b14dbc0c4e5493e743798e352739
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-03 10:51:39 -0600

    day 3 problem 1

---
+++ solutions/day-3-problem-1.go
@@ -0,0 +1,117 @@
+package solutions
+
+import (
+	"regexp"
+	"strconv"
+)
+
+func Day3Problem1(inputs []string) string {
+	var total int
+
+	for index, input := range inputs {
+		above := ""
+		if index > 0 {
+			above = inputs[index-1]
+		}
+		below := ""
+		if len(inputs) > index+1 {
+			below = inputs[index+1]
+		}
+		_, rowTotal := Day3Row(input).ParseNumbersWithAdjacent(above, below)
+		total += rowTotal
+	}
+
+	return strconv.Itoa(total)
+}
+
+type Day3Row string
+
+func (r Day3Row) ParseNumbersWithAdjacent(rowAbove string, rowBelow string) (nums []int, total int) {
+	if r == "" {
+		return
+	}
+
+	numbers := r.ParseNumbersFromRow()
+	for _, number := range numbers {
+		number.SetAdjacent(string(r), rowAbove, rowBelow)
+		if number.HasAdjacentSpecial() {
+			nums = append(nums, number.Number())
+			total += number.Number()
+		}
+	}
+
+	return
+}
+
+type Day3Number struct {
+	Raw        string
+	Index      int
+	CharsAbove string
+	CharLeft   string
+	CharRight  string
+	CharsBelow string
+}
+
+func (n Day3Number) Number() int {
+	i, _ := strconv.Atoi(n.Raw)
+	return i
+}
+
+func (n Day3Number) Len() int {
+	return len(n.Raw)
+}
+
+func (n Day3Number) HasAdjacentSpecial() bool {
+	adjacent := n.CharsAbove + n.CharLeft + n.CharRight + n.CharsBelow
+
+	reg, _ := regexp.Compile(`[^0-9\.]`)
+
+	return reg.Match([]byte(adjacent))
+}
+
+func (n *Day3Number) SetAdjacent(current string, above string, below string) {
+	adjacentLength := n.Len()
+	adjacentStartIndex := n.Index
+	if n.Index > 0 {
+		adjacentLength++
+		adjacentStartIndex--
+		n.CharLeft = string(current[n.Index-1])
+	}
+
+	if n.Index+n.Len() < len(current) {
+		adjacentLength++
+		n.CharRight = string(current[n.Index+n.Len()])
+	}
+
+	adjacentEndIndex := adjacentStartIndex + adjacentLength
+	if len(current) == len(above) {
+		n.CharsAbove = above[adjacentStartIndex:adjacentEndIndex]
+	}
+
+	if len(current) == len(below) {
+		n.CharsBelow = below[adjacentStartIndex:adjacentEndIndex]
+	}
+}
+
+func (r Day3Row) ParseNumbersFromRow() []Day3Number {
+	nums := make([]Day3Number, 0)
+
+	var current Day3Number
+	for index := range r {
+		ch := r[index]
+		if isInt(ch) {
+			if current.Raw == "" {
+				current.Index = index
+			}
+			current.Raw += string(ch)
+		} else if current.Raw != "" {
+			nums = append(nums, current)
+			current = Day3Number{}
+		}
+		if index == len(r)-1 && current.Raw != "" {
+			nums = append(nums, current)
+		}
+	}
+
+	return nums
+}
---
+++ solutions/day-3-problem-1_test.go
@@ -0,0 +1,129 @@
+package solutions_test
+
+import (
+	"advent/solutions"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type day3Datum struct {
+	line    string
+	numbers []solutions.Day3Number
+}
+
+var day3Data = []day3Datum{
+	{
+		line: "............................................411.....................363..134.........463.775..........................506...................",
+		numbers: []solutions.Day3Number{
+			{Raw: "411", Index: 44, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "...*."},
+			{Raw: "363", Index: 68, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "....*"},
+			{Raw: "134", Index: 73, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "...=."},
+			{Raw: "463", Index: 85, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "....*"},
+			{Raw: "775", Index: 89, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "*...."},
+			{Raw: "506", Index: 118, CharsAbove: "", CharLeft: ".", CharRight: ".", CharsBelow: "./..."},
+		},
+	},
+	{
+		line: "......429...836..$............../..960........*.............+..........*...=....381.....*........67......426.....=..../...304...............",
+		numbers: []solutions.Day3Number{
+			{Raw: "429", Index: 6, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "...72"},
+			{Raw: "836", Index: 12, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "960", Index: 35, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "381", Index: 80, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "3&../"},
+			{Raw: "67", Index: 97, CharsAbove: "....", CharLeft: ".", CharRight: ".", CharsBelow: "696."},
+			{Raw: "426", Index: 105, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "..131"},
+			{Raw: "304", Index: 122, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "....="},
+		},
+	},
+	{
+		line: "........721........*...17................790...670.............$..........893.93&../...129$.651.696.......131*99.............=......446*781.",
+		numbers: []solutions.Day3Number{
+			{Raw: "721", Index: 8, CharsAbove: "29...", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "17", Index: 23, CharsAbove: "....", CharLeft: ".", CharRight: ".", CharsBelow: "...."},
+			{Raw: "790", Index: 41, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "670", Index: 47, CharsAbove: "*....", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "893", Index: 74, CharsAbove: "..=..", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "93", Index: 78, CharsAbove: "...3", CharLeft: ".", CharRight: "&", CharsBelow: "...."},
+			{Raw: "129", Index: 87, CharsAbove: "..*..", CharLeft: ".", CharRight: "$", CharsBelow: "....."},
+			{Raw: "651", Index: 92, CharsAbove: ".....", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "696", Index: 96, CharsAbove: "..67.", CharLeft: ".", CharRight: ".", CharsBelow: "....."},
+			{Raw: "131", Index: 106, CharsAbove: "426..", CharLeft: ".", CharRight: "*", CharsBelow: "....."},
+			{Raw: "99", Index: 110, CharsAbove: "....", CharLeft: "*", CharRight: ".", CharsBelow: "...."},
+			{Raw: "446", Index: 132, CharsAbove: ".....", CharLeft: ".", CharRight: "*", CharsBelow: "....."},
+			{Raw: "781", Index: 136, CharsAbove: ".....", CharLeft: "*", CharRight: ".", CharsBelow: "...*9"},
+		},
+	},
+	{
+		line: "1$........................................................................................................................................*9",
+		numbers: []solutions.Day3Number{
+			{Raw: "1", Index: 0, CharsAbove: "..", CharLeft: "", CharRight: "$", CharsBelow: ""},
+			{Raw: "9", Index: 139, CharsAbove: "1.", CharLeft: "*", CharRight: "", CharsBelow: ""},
+		},
+	},
+}
+
+func Test_Day3Number_ParseNumbersFromRow(t *testing.T) {
+	for _, data := range day3Data {
+		test_Day3Number_ParseNumbersFromRow(t, data)
+	}
+}
+
+func test_Day3Number_ParseNumbersFromRow(t *testing.T, data day3Datum) {
+	nums := solutions.Day3Row(data.line).ParseNumbersFromRow()
+
+	assert.Equal(t, len(data.numbers), len(nums))
+	fmt.Println(nums)
+
+	for index, num := range data.numbers {
+		assert.Equal(t, num.Index, nums[index].Index, "Index not equal")
+		assert.Equal(t, num.Raw, nums[index].Raw, "Raw string not equal")
+		assert.Equal(t, num.Number(), nums[index].Number(), "Parsed int not equal")
+		assert.Equal(t, num.Len(), nums[index].Len(), "Raw length not equal")
+	}
+}
+
+func Test_Day3Number_SetAdjacent(t *testing.T) {
+	for index, data := range day3Data {
+		above := ""
+		if index > 0 {
+			above = day3Data[index-1].line
+		}
+		below := ""
+		if len(day3Data) > index+1 {
+			below = day3Data[index+1].line
+		}
+		test_Day3Number_SetAdjacent(t, data, above, below)
+	}
+}
+
+func test_Day3Number_SetAdjacent(t *testing.T, data day3Datum, above string, below string) {
+	for _, expected := range data.numbers {
+		actual := solutions.Day3Number{Raw: expected.Raw, Index: expected.Index}
+		actual.SetAdjacent(data.line, above, below)
+
+		assert.Equal(t, expected.CharsAbove, actual.CharsAbove, fmt.Sprintf("CharsAbove not equal: [%d]%s", expected.Index, expected.Raw))
+		assert.Equal(t, expected.CharLeft, actual.CharLeft, fmt.Sprintf("CharLeft not equal: [%d]%s", expected.Index, expected.Raw))
+		assert.Equal(t, expected.CharRight, actual.CharRight, fmt.Sprintf("CharRight not equal: [%d]%s", expected.Index, expected.Raw))
+		assert.Equal(t, expected.CharsBelow, actual.CharsBelow, fmt.Sprintf("CharsBelow not equal: [%d]%s", expected.Index, expected.Raw))
+	}
+}
+
+func Test_Day3Number_HasAdjacentSpecial(t *testing.T) {
+	assert.True(t, solutions.Day3Number{CharsAbove: "..*."}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharLeft: "*"}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharRight: "*"}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharsBelow: "..*.."}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharsBelow: "..$.."}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharsBelow: "../.."}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharsBelow: "..=.."}.HasAdjacentSpecial())
+	assert.True(t, solutions.Day3Number{CharsBelow: "..+.."}.HasAdjacentSpecial())
+
+	assert.False(t, solutions.Day3Number{}.HasAdjacentSpecial())
+	assert.False(t, solutions.Day3Number{CharsAbove: "..34.."}.HasAdjacentSpecial())
+	assert.False(t, solutions.Day3Number{CharLeft: "."}.HasAdjacentSpecial())
+	assert.False(t, solutions.Day3Number{CharRight: "."}.HasAdjacentSpecial())
+	assert.False(t, solutions.Day3Number{CharsBelow: "5...6"}.HasAdjacentSpecial())
+	assert.False(t, solutions.Day3Number{CharsAbove: "..34..", CharLeft: ".", CharRight: ".", CharsBelow: "5...6"}.HasAdjacentSpecial())
+}
