d6f8cd814f78e5f452d88a29cb37454e72d77733
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-07 09:08:38 -0600

    day 7 problem 1

---
+++ solutions/2023/day-7-problem-1.go
@@ -0,0 +1,156 @@
+package twentythree
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Day7Problem1(inputs []string) string {
+	hands := Day7ParseHands(inputs)
+
+	sort.Slice(hands, func(i, j int) bool {
+		left := hands[i]
+		right := hands[j]
+
+		if left.Type == right.Type {
+			for index := range left.Hand {
+				leftStr := CardStrength(left.Hand[index])
+				rightStr := CardStrength(right.Hand[index])
+
+				if leftStr != rightStr {
+					return leftStr < rightStr
+				}
+			}
+		}
+
+		return left.Type < right.Type
+	})
+
+	var total int
+	for index, hand := range hands {
+		total += (index + 1) * hand.Bet
+	}
+
+	return strconv.Itoa(total)
+}
+
+type Day7HandType int
+
+const (
+	Day7HighCard  Day7HandType = iota
+	Day7OnePair   Day7HandType = iota
+	Day7TwoPair   Day7HandType = iota
+	Day7ThreeKind Day7HandType = iota
+	Day7FullHouse Day7HandType = iota
+	Day7FourKind  Day7HandType = iota
+	Day7FiveKind  Day7HandType = iota
+)
+
+type Day7CardStrength int
+
+const (
+	Day7Two   Day7CardStrength = iota
+	Day7Three Day7CardStrength = iota
+	Day7Four  Day7CardStrength = iota
+	Day7Five  Day7CardStrength = iota
+	Day7Six   Day7CardStrength = iota
+	Day7Seven Day7CardStrength = iota
+	Day7Eight Day7CardStrength = iota
+	Day7Nine  Day7CardStrength = iota
+	Day7Ten   Day7CardStrength = iota
+	Day7Jack  Day7CardStrength = iota
+	Day7Queen Day7CardStrength = iota
+	Day7King  Day7CardStrength = iota
+	Day7Ace   Day7CardStrength = iota
+)
+
+func CardStrength(ch byte) Day7CardStrength {
+	switch ch {
+	case 'A':
+		return Day7Ace
+	case 'K':
+		return Day7King
+	case 'Q':
+		return Day7Queen
+	case 'J':
+		return Day7Jack
+	case 'T':
+		return Day7Ten
+	case '9':
+		return Day7Nine
+	case '8':
+		return Day7Eight
+	case '7':
+		return Day7Seven
+	case '6':
+		return Day7Six
+	case '5':
+		return Day7Five
+	case '4':
+		return Day7Four
+	case '3':
+		return Day7Three
+	case '2':
+		return Day7Two
+	default:
+		return Day7Two
+	}
+}
+
+func Day7ParseHands(inputs []string) (hands []Day7Hand) {
+	for _, input := range inputs {
+		hands = append(hands, Day7ParseHand(input))
+	}
+	return
+}
+
+func Day7ParseHand(input string) (hand Day7Hand) {
+	parts := strings.Split(input, " ")
+
+	hand.Hand = parts[0]
+	hand.Bet, _ = strconv.Atoi(parts[1])
+	hand.ParseType()
+
+	return
+}
+
+type Day7Hand struct {
+	Hand string
+	Bet  int
+	Type Day7HandType
+}
+
+func (h *Day7Hand) ParseType() {
+	cards := make(map[rune]int)
+
+	for _, ch := range h.Hand {
+		cards[ch] = cards[ch] + 1
+	}
+
+	switch len(cards) {
+	case 1: // only one card means all are the same and is a "five of a kind"
+		h.Type = Day7FiveKind
+	case 2: // two different cards means it can either be a "full house" or "four of a kind"
+		for _, count := range cards {
+			if count == 2 || count == 3 {
+				h.Type = Day7FullHouse
+			} else {
+				h.Type = Day7FourKind
+			}
+			break
+		}
+	case 3: // three different cards means it can either be a "three of a kind" or "two pair"
+		h.Type = Day7TwoPair
+		for _, count := range cards {
+			if count == 3 {
+				h.Type = Day7ThreeKind
+				break
+			}
+		}
+	case 4: // 4 different cards means it is a "one pair"
+		h.Type = Day7OnePair
+	default: // default or 5 different cards means none match and is simply "high card"
+		h.Type = Day7HighCard
+	}
+}
