d01e20038017d575f64eab1fd94b3a3258460718
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-08 23:26:28 -0600

    day 9 problem 1

--- solutions/2023/day-9-problem-1.go
+++ solutions/2023/day-9-problem-1.go
@@ -1,5 +1,52 @@
 package twentythree
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Day9Problem1(inputs []string) string {
-	return ""
+	var total int
+	for _, input := range inputs {
+		pattern := Day9Pattern(ParseInts(input))
+		next := pattern.Next()
+		total += next
+	}
+
+	return strconv.Itoa(total)
+}
+
+type Day9Pattern []int
+
+func (p Day9Pattern) Next() int {
+	nextPattern := make(Day9Pattern, 0)
+	var numZeroes int
+
+	for index := range p {
+		if index+1 < len(p) {
+			difference := p[index+1] - p[index]
+			nextPattern = append(nextPattern, difference)
+			if difference == 0 {
+				numZeroes++
+			}
+		}
+	}
+
+	if numZeroes == len(p)-1 {
+		return p[len(p)-1]
+	}
+
+	return p[len(p)-1] + nextPattern.Next()
+}
+
+func ParseInts(input string) (nums []int) {
+	parts := strings.Split(input, " ")
+
+	for _, part := range parts {
+		if num, err := strconv.Atoi(part); err == nil {
+			nums = append(nums, num)
+		}
+	}
+
+	return
 }
