bdfff28d686ee360a79b922c4d8bffec7c7617d2
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-12 21:16:11 -0600

    day 12 problem 1

--- solutions/2023/day-12-problem-1.go
+++ solutions/2023/day-12-problem-1.go
@@ -1,5 +1,111 @@
 package twentythree
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Day12Problem1(inputs []string) string {
-	return "1"
+	rows := Day12ParseInput(inputs)
+
+	var sum int
+	for _, row := range rows {
+		sum += row.FindPossibilities()
+	}
+
+	return strconv.Itoa(sum)
+}
+
+type Day12Row struct {
+	Springs      string
+	StatusGroups []int
+}
+
+func (r Day12Row) FindPossibilities() (count int) {
+	filled := Day12FillUnknownsRecursive(r.Springs)
+
+	for _, fill := range filled {
+		if r.IsFilledValid(fill) {
+			// fmt.Printf("%s => %s, %v\n", r.Springs, fill, r.StatusGroups)
+			count++
+		}
+	}
+
+	return
+}
+
+func (r Day12Row) IsFilledValid(str string) bool {
+	counts := make([]int, 0)
+
+	var count int
+	for index, ch := range str {
+		if ch == '#' {
+			count++
+			if index == len(str)-1 {
+				counts = append(counts, count)
+			}
+		} else if count == 0 {
+			continue
+		} else {
+			counts = append(counts, count)
+			count = 0
+		}
+	}
+
+	// fmt.Printf("%s => %s | %v == %v\n", r.Springs, str, counts, r.StatusGroups)
+
+	if len(counts) != len(r.StatusGroups) {
+		return false
+	}
+
+	for index := range counts {
+		if counts[index] != r.StatusGroups[index] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Day12FillUnknownsRecursive(str string) (filled []string) {
+	var next []string
+	if len(str) > 1 {
+		next = Day12FillUnknownsRecursive(str[1:])
+	} else if len(str) == 0 {
+		return
+	} else {
+		next = append(next, "")
+	}
+
+	for _, n := range next {
+		if str[0] == '?' {
+			filled = append(filled, "."+n, "#"+n)
+		} else {
+			filled = append(filled, string(str[0])+n)
+		}
+	}
+
+	return
+}
+
+func Day12ParseInput(inputs []string) (rows []Day12Row) {
+	for _, input := range inputs {
+		rows = append(rows, Day12ParseRow(input))
+	}
+	return
+}
+
+func Day12ParseRow(input string) (row Day12Row) {
+	parts := strings.Split(input, " ")
+
+	row.Springs = parts[0]
+
+	nums := strings.Split(parts[1], ",")
+	for _, num := range nums {
+		if x, err := strconv.Atoi(num); err == nil {
+			row.StatusGroups = append(row.StatusGroups, x)
+		}
+	}
+
+	return
 }
---
+++ solutions/2023/day-12-problem-1_test.go
@@ -0,0 +1,28 @@
+package twentythree_test
+
+import (
+	twentythree "advent/solutions/2023"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Day12RowTest struct {
+	Row          twentythree.Day12Row
+	Arrangements int
+}
+
+func Test_FindPossibilities(t *testing.T) {
+	rows := []Day12RowTest{
+		{Arrangements: 1, Row: twentythree.Day12Row{Springs: "???.###", StatusGroups: []int{1, 1, 3}}},
+		{Arrangements: 4, Row: twentythree.Day12Row{Springs: ".??..??...?##.", StatusGroups: []int{1, 1, 3}}},
+		{Arrangements: 1, Row: twentythree.Day12Row{Springs: "?#?#?#?#?#?#?#?", StatusGroups: []int{1, 3, 1, 6}}},
+		{Arrangements: 1, Row: twentythree.Day12Row{Springs: "????.#...#...", StatusGroups: []int{4, 1, 1}}},
+		{Arrangements: 4, Row: twentythree.Day12Row{Springs: "????.######..#####.", StatusGroups: []int{1, 6, 5}}},
+		{Arrangements: 10, Row: twentythree.Day12Row{Springs: "?###????????", StatusGroups: []int{3, 2, 1}}},
+	}
+
+	for index, row := range rows {
+		assert.Equal(t, row.Arrangements, row.Row.FindPossibilities(), index)
+	}
+}
