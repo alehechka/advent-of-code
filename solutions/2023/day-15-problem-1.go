e78bff162adeb401d83512de6ed0b3cea823145e
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-15 08:31:24 -0600

    day 15 problem 1

--- solutions/2023/day-15-problem-1.go
+++ solutions/2023/day-15-problem-1.go
@@ -1,5 +1,26 @@
 package twentythree
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Day15Problem1(inputs []string) string {
-	return "1"
+	codes := strings.Split(inputs[0], ",")
+
+	var sum int
+	for _, code := range codes {
+		sum += Day15HASH(code)
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func Day15HASH(str string) (value int) {
+	for _, ch := range str {
+		value += int(ch)
+		value *= 17
+		value %= 256
+	}
+	return
 }
