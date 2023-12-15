c573d6b21c38bc287c8b7fff4789f8e9b54c5281
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-14 20:36:35 -0600

    enhanced day-12-problem-2

--- solutions/2023/day-12-problem-2.go
+++ solutions/2023/day-12-problem-2.go
@@ -11,6 +11,60 @@ func Day12Problem2(inputs []string) string {
 	rows := Day12ParseInputUnfolded(inputs)
 
 	var sum int
+	// fmt.Println(rows[len(rows)-1].MatchesTotal())
+	for _, row := range rows {
+		sum += row.MatchesTotal()
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func (row Day12Row) MatchesTotal() int {
+	states := "."
+	for _, num := range row.StatusGroups {
+		states += strings.Repeat("#", num)
+		states += "."
+	}
+
+	states_dict := map[int]int{0: 1}
+	new_dict := make(map[int]int)
+
+	for _, ch := range row.Springs {
+		for state := range states_dict {
+			switch ch {
+			case '?':
+				if state+1 < len(states) {
+					new_dict[state+1] = new_dict[state+1] + states_dict[state]
+				}
+				if states[state] == '.' {
+					new_dict[state] = new_dict[state] + states_dict[state]
+				}
+			case '.':
+				if state+1 < len(states) && states[state+1] == '.' {
+					new_dict[state+1] = new_dict[state+1] + states_dict[state]
+				}
+				if states[state] == '.' {
+					new_dict[state] = new_dict[state] + states_dict[state]
+				}
+			case '#':
+				if state+1 < len(states) && states[state+1] == '#' {
+					new_dict[state+1] = new_dict[state+1] + states_dict[state]
+				}
+			}
+		}
+
+		states_dict = new_dict
+		new_dict = make(map[int]int)
+	}
+
+	sum := states_dict[len(states)-1] + states_dict[len(states)-2]
+	return sum
+}
+
+func Day12Problem2_BruteForce(inputs []string) string {
+	rows := Day12ParseInputUnfolded(inputs)
+
+	var sum int
 	var wg sync.WaitGroup
 	for _, row := range rows {
 		wg.Add(1)
