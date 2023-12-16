940f2858734f0291079ba5c4e72e76faa1aa6ca3
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2023-12-16 09:54:21 -0600

    day 16 problem 2

--- solutions/2023/day-16-problem-2.go
+++ solutions/2023/day-16-problem-2.go
@@ -1,5 +1,49 @@
 package twentythree
 
+import (
+	"strconv"
+	"sync"
+)
+
 func Day16Problem2(inputs []string) string {
-	return "2"
+	grid := Day16Grid(inputs)
+
+	var wg sync.WaitGroup
+
+	var largest int
+	for y := range grid {
+		wg.Add(1)
+		go func(start int) {
+			if count := grid.CountEnergizedFromStart(0, start, Right); count > largest {
+				largest = count
+			}
+			if count := grid.CountEnergizedFromStart(len(grid[start])-1, start, Left); count > largest {
+				largest = count
+			}
+			wg.Done()
+		}(y)
+	}
+
+	for x := range grid[0] {
+		wg.Add(1)
+		go func(start int) {
+			if count := grid.CountEnergizedFromStart(start, 0, Down); count > largest {
+				largest = count
+			}
+			if count := grid.CountEnergizedFromStart(start, len(grid)-1, Up); count > largest {
+				largest = count
+			}
+			wg.Done()
+		}(x)
+	}
+
+	wg.Wait()
+
+	return strconv.Itoa(largest)
+}
+
+func (g Day16Grid) CountEnergizedFromStart(x, y int, direction rune) (total int) {
+	energized := make(Day16Energized)
+	g.fillEnergized(x, y, direction, energized)
+	return len(energized)
 }
--- solutions/2023/day-16_test.go
+++ solutions/2023/day-16_test.go
@@ -23,3 +23,7 @@ var day16input = []string{
 func Test_Day16Problem1(t *testing.T) {
 	assert.Equal(t, "46", twentythree.Day16Problem1(day16input))
 }
+
+func Test_Day16Problem2(t *testing.T) {
+	assert.Equal(t, "51", twentythree.Day16Problem2(day16input))
+}
